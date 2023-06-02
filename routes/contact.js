d7072ed7c98a4afa99c9669de37e201b2fa960ba
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-02 22:14:34 +0530

    JS file for admin,shop,contact, success

---
+++ routes/contact.js
@@ -0,0 +1,19 @@
+
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
+router.get('/contact',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contact.html'));
+});
+
+router.post('/contact',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/success');
+});
+
+module.exports=router;
